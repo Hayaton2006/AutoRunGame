7078ca200b60a2a0b18a07429c5f485a3df49e06
tomoya <tomoyamario29@gmail.com>
2025-11-17 13:29:12 +0900

    敵と馬車の体力、プレイヤーが当たった際にノックバックする仕様を実装した。

---
+++ Assets/Mutou/Scripts/ChaseState.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+
+public partial class Enemy
+{
+    class ChaseState : State
+    {
+        readonly Transform transform;
+        readonly GameObject target;
+        readonly float chaseSpeed;
+
+        public ChaseState(Transform transform, GameObject target, float chaseSpeed)
+        {
+            this.transform = transform;
+            this.target = target;
+            this.chaseSpeed = chaseSpeed;
+        }
+
+        public override bool CanTransition(Type currentStateType)
+        {
+           return true;
+        }
+
+        public override void Enter()
+        {
+
+        }
+
+        public override void Exit()
+        {
+ 
+        }
+
+        public override void Update()
+        {
+            transform.LookAt(target.transform);
+            transform.position += transform.forward * chaseSpeed;
+        }
+    }
+
+}
---
+++ Assets/Mutou/Scripts/Guard.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Guard : MonoBehaviour
+{
+    //[SerializeField]float speed;
+    //[SerializeField] GameObject target;
+    //[SerializeField] float detectionRange;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //target = GameObject.Find("Enemy");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.Translate(0,0,speed);
+        //float dis = Vector3.Distance(this.transform.position, target.transform.position);
+        //if (dis <= detectionRange)
+        //{
+        //    transform.LookAt(target.transform);
+        //    transform.position += transform.forward * speed;
+        //}
+    }
+}
